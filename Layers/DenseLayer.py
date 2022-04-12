85d5fd5d6f6d004ee5cf6b61b07285a8a2c6c0ea
swewalka-tgm <simicraft04@gmail.com>
2022-04-12 10:03:31 +0200

    iwas scheisö

---
+++ Layers/DenseLayer.py
@@ -0,0 +1,13 @@
+from typing_extensions import Self
+import numpy as np
+
+class DenseLayer():
+
+    units = np.empty()
+    activation = ''
+    imput_shape = []
+
+    def __init__(self):
+        self.units = np.empty(100,dtype=np.float64)
+
+    
